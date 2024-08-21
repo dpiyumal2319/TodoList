ef809f8f8c71ac45244f999d8edd23dbb65861c6
Dasun <w.piyumal2319@gmail.com>
2024-08-21 21:54:55 +0530

    Added ejs

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const request = require('request');
+let ejs = require('ejs');
 
 const app = express();
+
+app.use("view-engine", "ejs");
+
 app.use(bodyparser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    } else {
+        day = "Weekday";
+    }
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function() {
