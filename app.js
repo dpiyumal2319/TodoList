f5f42d4c2bb28ddbca9abeba705ad45b974d6cb9
Dasun <w.piyumal2319@gmail.com>
2024-08-21 22:36:15 +0530

    Make the code take something from browser

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+//Middleware to parse JSON bodies
+app.use(express.json());
+
+//Middleware to parse URL-encoded bodies
+app.use(express.urlencoded( {extended: true} ));
+
+var item = "";
 
 app.get('/', function(req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
-    }
-    res.render("list", {kindOfDay: day});
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    
+    res.render("list", {kindOfDay: day, newItem: item});
 });
+
+app.post('/', function(req, res) {
+    item = req.body.newItem;
+    res.redirect('/');
+})
 
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
