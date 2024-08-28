43837745b54e8b324eae6cc57712163c669f909d
dpiyumal2319 <132676592+dpiyumal2319@users.noreply.github.com>
2024-08-28 23:15:09 +0530

    Added adding default items

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 //Set the view engine to ejs
 app.set("view engine", "ejs");
@@ -9,39 +10,80 @@ app.set("view engine", "ejs");
 app.use(express.json());
 
 //Middleware to parse URL-encoded bodies
-app.use(express.urlencoded( {extended: true} ));
+app.use(express.urlencoded({ extended: true }));
 
 //Added public repository to serve static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+//Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-app.get('/', function(req, res) {
-    let day = date.getDay();  
-    res.render("list", {kindOfDay: day, newListItems: items});
+//Create a new schema
+const items = new mongoose.Schema({
+    name: String,
 });
 
-app.get('/work', function(req, res) {
-    res.render("list", {kindOfDay: "Work List", newListItems: workItems});
+//Create a new model
+const Item = mongoose.model("Item", items);
+
+//Create a new document
+const item1 = new Item({
+    name: "Welcome to your todolist!",
 });
 
-app.post('/', function(req, res) {
+const item2 = new Item({
+    name: "Hit the + button to add a new item.",
+});
+
+const defaultItems = [item1, item2];
+
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+
+app.get("/", function (req, res) {
+    let day = date.getDay();
+
+    //Find all items in the database
+    Item.find({})
+        .then((items) => {
+            if (items.length === 0) {
+                //Insert the default items into the database
+                Item.insertMany(defaultItems)
+                    .then(function () {
+                        console.log("Successfully inserted default items");
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+                res.redirect("/");
+            } else {
+                res.render("list", { kindOfDay: day, newListItems: items });
+            }
+        }).catch ((err) => {
+            console.log(err);
+        });
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { kindOfDay: "Work List", newListItems: workItems });
+});
+
+app.post("/", function (req, res) {
     let item = req.body.newItem;
 
-    if(req.body.button === "Work List") {
+    if (req.body.button === "Work List") {
         workItems.push(item);
-        res.redirect('/work');
-    } else{
+        res.redirect("/work");
+    } else {
         items.push(item);
-        res.redirect('/');
+        res.redirect("/");
     }
-})
+});
 
-app.get('/about', function(req, res) {
+app.get("/about", function (req, res) {
     res.render("about");
 });
 
-app.listen(3000, function() {
-    console.log('Server is running on port 3000');
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
 });
